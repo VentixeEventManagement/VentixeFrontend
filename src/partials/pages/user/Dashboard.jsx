28a6aa8ee3129d41184e16f7e521a9fadef9957a
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-04-23 18:50:22 +0200

    Implement user dashboard with temporary counter functionality

--- src/partials/pages/user/Dashboard.jsx
+++ src/partials/pages/user/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, incrementByAmount } from '../../../features/counterSlice';
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.counter.value);
+
+  const decreaseCurrenValue = (val) => {
+    if (val > 0) {
+      dispatch(decrement());
+      return val
+    }
+  };
+
   return (
-    <div>User Dashboard</div>
+    <div>
+      <h1>User Dashboard</h1>
+
+      <h3>Value: {value}</h3>
+      <button onClick={() => dispatch(increment())}>Increase</button>
+      <button onClick={() => decreaseCurrenValue(value)}>Decrease</button>
+      <button onClick={() => dispatch(incrementByAmount(value))}>Increase with current value</button>
+    </div>
   )
 }
 
