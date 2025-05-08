f8897bab89bb73e473a0d5235bdbb489d3409fb9
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-08 14:18:57 +0200

    feat: add SignUp functionality with async user registration and state management

--- src/partials/pages/auth/SignUp.jsx
+++ src/partials/pages/auth/SignUp.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SignUp.css"
+import { signUpUser } from '../../../features/SignUpSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const { loading, error, isAuthenticated } = useSelector((state) => state.signup)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(signUpUser({ email, password }))
+  }
 
   return (
     <div className='modal-wrapper'>
       <div className="card">
-        <form >
+        <form onSubmit={handleSubmit}>
           <h1>Sign up</h1>
           <div className="form-group">
-            <input type="email" id='email' placeholder='Email' />
+            <input type="email" id='email' placeholder='Email' onChange={handleEmailChange} required />
           </div>
           <div className="form-group">
-            <input type="password" id='password' placeholder='Password' />
+            <input type="password" id='password' placeholder='Password' onChange={handlePasswordChange} required />
           </div>
           <div className="form-group">
             <input type="password" id='confirmpassword' placeholder='Confirm password' />
           </div>
-          <button type='submit'>Sign up</button>
+          <button type='submit' disabled={loading}>Sign up</button>
+
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          {isAuthenticated && <p>You're signed up!</p>}
         </form>
       </div>
     </div>
