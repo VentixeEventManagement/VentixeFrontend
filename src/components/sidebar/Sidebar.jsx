be53cf5e42bf8f2e233b644afd1d0e6c927cd253
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-05-12 11:51:25 +0200

    syntax fix

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,75 +1,73 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import SignOutBtn from '../signOutBtn/SignOutBtn'
-
+import SignOutBtn from "../signOutBtn/SignOutBtn";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar-container-all-links">
-      <div className="sidebar-container-link">
-        <NavLink
-          className={({ isActive }) =>
-            isActive ? "sidebar-link active" : "sidebar-link"
-          }
-          to="/dashboard"
-        >
-          Dashboard
-        </NavLink>
+    <>
+      <div className="sidebar-container-all-links">
+        <div className="sidebar-container-link">
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "sidebar-link active" : "sidebar-link"
+            }
+            to="/dashboard"
+          >
+            Dashboard
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Bookings
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="/admin/invoices">
+            Invoices
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Inbox
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Calendar
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Events
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Financials
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Gallery
+          </NavLink>
+        </div>
+
+        <div className="sidebar-container-link">
+          <NavLink className="sidebar-link" to="">
+            Feedback
+          </NavLink>
+        </div>
       </div>
 
-
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Bookings
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="/admin/invoices">
-          Invoices
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Inbox
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Calendar
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Events
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Financials
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Gallery
-        </NavLink>
-      </div>
-
-      <div className="sidebar-container-link">
-        <NavLink className="sidebar-link" to="">
-          Feedback
-        </NavLink>
-      </div>
-    </div>
-
-
-    <SignOutBtn />
+      <SignOutBtn />
+    </>
   );
 };
 
