f6182affdc07eff58e88deba8617cb8ae32a12c2
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-05-16 10:26:35 +0200

    topbarfix-invoicespage

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "./Topbar.css";
 
 const Topbar = () => {
-    return (
-        <></>
-    )
-}
+  const location = useLocation();
 
-export default Topbar
+  const getTitle = (pathname) => {
+    switch (pathname) {
+      case "/admin/invoices":
+        return "Invoices";
+      //Fyll på med varje path
+    }
+  };
+  return (
+    <>
+      <div className="topbar">
+        <div className="topbar-path">
+          Dashboard / {getTitle(location.pathname)}
+        </div>
+        <h1 className="topbar-header">{getTitle(location.pathname)}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Topbar;
