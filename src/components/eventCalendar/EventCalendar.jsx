56b723d6e3342e98339f1782c133b431883eb4c4
Martin Mohns <mamoh55@gmail.com>
2025-05-22 12:59:29 +0200

    Added React Calendar

---
+++ src/components/eventCalendar/EventCalendar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+  getDay,
+  locales,
+});
+
+function EventCalendar() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("https://eventprovider20250513014429-abhdcxdthwhpc3dp.swedencentral-01.azurewebsites.net/api/Event")
+      .then((res) => res.json())
+      .then((data) => {
+        const mappedEvents = data.map((event) => ({
+          title: event.name,
+          start: new Date(event.startDate),
+          end: new Date(event.endDate),
+          allDay: false,
+        }));
+        setEvents(mappedEvents);
+      });
+  }, []);
+
+  return (
+    <div style={{ height: 500 }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+}
+
+export default EventCalendar;
--- src/partials/pages/user/Calendar.jsx
+++ src/partials/pages/user/Calendar.jsx
@@ -1,49 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import { format, parse, startOfWeek, getDay } from "date-fns";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import React from "react";
+import EventCalendar from "../../../components/eventCalendar/EventCalendar.jsx";
 
-const locales = {
-  "en-US": require("date-fns/locale/en-US"),
+const Calendar = () => {
+    return (
+        <div>
+            <h1>Events Page</h1>
+            <div>
+                <EventCalendar />
+            </div>
+        </div>
+    );
 };
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
-  getDay,
-  locales,
-});
-
-function MyCalendar() {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    fetch("https://eventprovider20250513014429-abhdcxdthwhpc3dp.swedencentral-01.azurewebsites.net/api/Event")
-      .then((res) => res.json())
-      .then((data) => {
-        // Map backend events to calendar format
-        const mappedEvents = data.map((event) => ({
-          title: event.name,
-          start: new Date(event.startDate),
-          end: new Date(event.endDate),
-          allDay: false,
-        }));
-        setEvents(mappedEvents);
-      });
-  }, []);
-
-  return (
-    <div style={{ height: 500 }}>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: "100%" }}
-      />
-    </div>
-  );
-}
-
-export default MyCalendar;
+export default Calendar;
