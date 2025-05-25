ea5dbdef899ca92eb47b34613ec598a7015e5dac
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-25 11:09:44 +0200

    Enhance MobileTopbar functionality: add dynamic title based on route and improve CSS structure for better layout.

--- src/components/mobileTopbar/MobileTopbar.jsx
+++ src/components/mobileTopbar/MobileTopbar.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import HamburgerIcon from "../hamburgerIcon/HamburgerIcon"
-import "./MobileTopbar.css"
 import MobileMenu from "../mobileMenu/MobileMenu";
+import { useLocation } from "react-router-dom";
+import "./MobileTopbar.css"
 
 const MobileTopbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const location = useLocation();
 
     const toggleMenu = () => {
         setIsMenuOpen((prevState) => !prevState);
@@ -13,6 +15,27 @@ const MobileTopbar = () => {
     const closeMenu = () => {
         setIsMenuOpen(false);
     };
+
+    const getTitle = (pathname) => {
+
+        switch (pathname) {
+            // Admin user pages
+            case "/admin/dashboard":
+                return "Dashboard";
+            case "/admin/invoices":
+                return "Invoices";
+
+            // No admin user pages
+            case "/dashboard":
+                return "Dashboard";
+            case "/profile":
+                return "Profile"
+
+            //Fyll på med varje path
+        }
+    };
+
+
     return (
         <div className="app-wrapper">
             <div className="mobile-topbar-wrapper">
@@ -20,8 +43,8 @@ const MobileTopbar = () => {
                     <div className="menu-logo">
                         <img src="/icons/tabletLogo.svg" alt="Logo icon" />
                     </div>
-                    <div className="menu-page-label">
-                        label
+                    <div className="menu-page-header">
+                        <h1 className="topbar-header">{getTitle(location.pathname)}</h1>
                     </div>
                     <HamburgerIcon onClicked={toggleMenu} isActive={isMenuOpen} />
                 </div>
