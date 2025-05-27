b507bfca275e0ed71a1818cbeaa502452c1cac9d
Martin Mohns <mamoh55@gmail.com>
2025-05-27 06:16:59 +0200

    Removed Proptype

--- src/components/eventAdminButtons/eventAdminButtons.jsx
+++ src/components/eventAdminButtons/eventAdminButtons.jsx
@@ -4,17 +4,20 @@ import CreateEventForm from "./createEventForm/createEventForm";
 import { useDispatch } from "react-redux";
 import { createEvent } from "../../features/eventsSlice";
 
-const EventAdminButtons = ({ onUpdate, onDelete }) => {
+const EventAdminButtons = () => {
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleCreate = (data) => {
     dispatch(createEvent(data));
+    setOpen(false);
   };
 
   return (
-    <div className="event-admin-buttons" >
-      <button type="button" onClick={() => setOpen(true)}>Create Event</button>
+    <div className="event-admin-buttons">
+      <button onClick={() => setOpen(true)}>
+        Create Event
+      </button>
       <CreateEventForm
         open={open}
         onClose={() => setOpen(false)}
