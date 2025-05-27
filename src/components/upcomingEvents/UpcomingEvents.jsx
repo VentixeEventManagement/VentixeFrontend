3348988b7ea2f855d3d4ede83433317de76d4a59
Martin Mohns <mamoh55@gmail.com>
2025-05-27 07:11:48 +0200

    Added CSS styling

--- src/components/upcomingEvents/UpcomingEvents.jsx
+++ src/components/upcomingEvents/UpcomingEvents.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchEvents } from "../../features/eventsSlice";
+import "./UpcomingEvents.css"; 
 
 const UpcomingEvents = () => {
   const dispatch = useDispatch();
