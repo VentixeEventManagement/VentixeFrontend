a0c50b0ea173aea201c9c99b1369cf84506c0e0d
Martin Mohns <mamoh55@gmail.com>
2025-05-27 05:08:01 +0200

    Removed Events title

--- src/partials/pages/user/Events.jsx
+++ src/partials/pages/user/Events.jsx
@@ -4,7 +4,6 @@ import EventViewer from "../../../components/eventViewer/EventViewer.jsx";
 const Events = () => {
     return (
         <div>
-            <h1>Events Page</h1>
             <div>
                 <EventViewer />
             </div>
